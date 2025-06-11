e74f313ad8962926c324be3f8ef8adc2a8cc443e
ggj0228 <ggj0228@naver.com>
2025-06-11 13:59:20 +0900

    C03Operator

---
+++ src/C03Operator.java
@@ -0,0 +1,76 @@
+public class C03Operator {
+    public static void main(String[] args) {
+//      산술연산자 : 사칙연산
+//        int n1 = 8;
+//        int n2 = 3;
+//      *: 곱셉 |  /: 나눗셈(몫) | %: 나머지
+//        System.out.println("n1 * n2 = " + (n1*n2));
+//        System.out.println("n1 * n2 = " + (n1/n2));
+//        System.out.println("n1 * n2 = " + (n1%n2));
+
+
+//      대입연산자
+//        int n1 = 7;
+//        int n2 = 7;
+//        int n3 = 7;
+//        n1 = n1 -3;
+//        n2 -= 3;
+//        n3 = -3;
+//        System.out.println(n1);
+//        System.out.println(n2);
+//        System.out.println(n3);
+//
+//        int n4 = 10;
+//        int n5 = 10;
+//        n4 /= 3;
+//        n5 %= 3;
+//        System.out.println(n4);
+//        System.out.println(n5);
+
+
+
+//      증감연산자
+//        int a = 10;
+//        a = a + 1;
+//        System.out.println(a); // 11
+//        a += 1;
+//        System.out.println(a); // 12
+//        a++;    //  후위증감연산자
+//        System.out.println(a); // 13
+//        ++a;
+//        System.out.println(a); // 14
+
+//      a++ :  현재 라인의 명령문이 실행된 후 증가
+//      ++a :  현재 라인의 명령문이 실행되기 전 증가
+//      별도의 명령문으로 따로 쓰기
+//        int a = 10;
+//        System.out.println(a);
+//
+//        int b = 5;
+//        int c = b++;    // c: 5
+//        int d = ++b;    // d: 7
+
+//     비교연산자 : ==, !=, >, >= 등
+//        char ch1 = 'a';
+//        char ch2 = 'b';
+//        System.out.println(ch1 == ch2);     // false
+//        System.out.println(ch1 != ch2);     // true
+
+//       논리연산자 : &&, ||, !
+//        int num1 = 10;
+//        int num2 = 20;
+//        boolean b1 = num1>5 && num1 < 20; // b1의 값 ture
+//        System.out.println(b1);
+//      num2가 10과 30사이에 있는지 여부를 b2에 값 세팅
+//        boolean b2 = !(num2 < 10 || num2 > 30);
+//        boolean b3 = num2 >= 10 && num2 < 30;
+//        System.out.println(b2);
+//        System.out.println(b3);
+
+//      비트연산자   :   컴퓨터의 2진체계에서의 연산방식. &, |, ^, <<, >>
+
+
+
+
+    }
+}
