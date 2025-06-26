003834a029d277a56df80275311c85e07f5b93f4
우영짱 <wooyoungking@192-0-200-7.cpe.teksavvy.com>
2025-06-26 10:15:09 +0900

    checked Exception ++

--- src/C07ExceptionFileParsing/C01Exception.java
+++ src/C07ExceptionFileParsing/C01Exception.java
@@ -1,6 +1,11 @@
 package C07ExceptionFileParsing;
 
 import java.io.*;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.rmi.RemoteException;
+import java.security.InvalidParameterException;
 import java.sql.SQLException;
 import java.util.*;
 
@@ -39,41 +44,65 @@ public class C01Exception {
 
         // 의도된 예외 강제 발생 : 특정 시점에 프로그램을 강제 중지
 
-        System.out.println("로그인을 위한 비밀번호 입력");
-        Scanner sc = new Scanner(System.in);
-        String input = sc.nextLine();
+//        System.out.println("로그인을 위한 비밀번호 입력");
+//        Scanner sc = new Scanner(System.in);
+//        String input = sc.nextLine();
 //        login(input);
 //        System.out.println("환영합니다");
 
-        // checked exception의 경우에는 예외처리를 위임받게되면, 반드시 예외처리를 하거나, 다시 throws해야함.
-
+        // checked exception의 경우
+        // 1) 호출받는 쪽에서 예외처리 후 unchecked exception 예외 발생. 호출하는 쪽에서 예외처리하여 사용자에겍 적절한 message 전달.
+        String result = null;
         try {
-            login2(input);
-        } catch (SQLException e) {
+            result = fileRead("/Users/wooyoungking/Workspace/Beyond_SW/text.txt");
+        } catch(RuntimeException e) {
+            System.out.println(e.getMessage());
             e.printStackTrace();
         }
-        System.out.println("환영합니다.");
+        // 2) throws를 통해서 checked예외처리를 위임받게되면, throws가 의미 있으며
+//        String result = null;
+//        try {
+//           result = fileRead("/Users/wooyoungking/Workspace/Beyond_SW/text.txt");
+//        } catch (IOException e) {
+//            throw new RuntimeException("입출력 과정에서 예외가 발생했습니다");
+//        }
+        System.out.println(result);
+
     }
+
 
     // throws 키워드를 통해 예외를 호출하는 측에 위임.
     // 다만, unchecked예외에서는 예외처리가 강제가 아니므로 throws가 큰 의미는 업다!
     // 그러나, checked예외에서는 예외처리가 강제되므로, theows가 의미 있음.
-    static boolean login(String password) throws IllegalArgumentException {
+    static boolean login(String password) throws IllegalArgumentException, NoSuchElementException {
         if(password.equals("1234")){
             return true;
-        } else {
+        } else if (password == null){
             // 예외를 강제 발생시킴으로써 이 시점에서 해당 메서드 강제 종료
             // 예외는 기본적으로 해당 메서드를 호출한 쪽으로 전파
-            throw new IllegalArgumentException("비밀번호를 잘못 입력하였습니다.");
+            throw new IllegalArgumentException("비밀번호를 잘못 입력하셨습니다");
+        } else  {
+            throw new  NoSuchElementException("그러한 비밀번호는 없습니다.");
         }
     }
-    static boolean login2(String password) throws SQLException {
-        if(password.equals("1234")){
-            return true;
-        } else {
-            // checked exception은 예외처리가 강제됨
-            //해당 메서드 내에서 예외처리를 하든, 아니면 명시적으로 throws를 통해 호출 메서드쪽에 위임.
-            throw new SQLException("비밀번호를 잘못 입력하였습니다.");
+
+    // checked 예시
+    static String fileRead(String path) throws RuntimeException {
+            Path filePath = Paths.get(path);
+//            // checked Exception의 경우 예외처리가 강제된다
+//            // 방법 1. checked를 try catch한 이후에 unchecked예외를 다시 throw
+        String text = null;
+        try {
+             text = Files.readString(filePath);
+        } catch (IOException e) {
+//            // 보통은 코드 중지 목적으로 unchecked Exception으로 다시 throw
+            throw new RuntimeException("입출력 과정에서 예외가 발생했습니다.");
         }
+
+        // 방법 2. 현재 메서드를 호출하는 쪽으로 예외를 위임(throws)
+//        String text = Files.readString(filePath);
+        return text;
     }
 }
+
+
